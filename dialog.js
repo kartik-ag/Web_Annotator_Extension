a6ec2f8210843a07a202feafb47d1c1aa5249e92
Kartik <agarwalkartik2018@gmail.com>
2024-06-02 13:25:46 +0530

    eraser added

--- dialog.js
+++ dialog.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function setupSliders() {
   let sliders = document.querySelectorAll('.unique-slider-input');
-  let values = document.querySelectorAll('.unique-value');
-  sliders.forEach((slider, index) => {
+  sliders.forEach((slider) => {
     slider.addEventListener('input', (e) => {
-      values[index].textContent = e.target.value + '%';
+      let value = slider.parentElement.querySelector('.unique-value');
+      value.textContent = e.target.value + '%';
     });
   });
 }
@@ -30,6 +30,7 @@ function handleOnClickUnique(clickedButton) {
   const isArrow = clickedButton.classList.contains('my-arrow');
   const isLine = clickedButton.classList.contains('my-line');
   const isPen = clickedButton.classList.contains('my-pen');
+  const isErase = clickedButton.classList.contains('my-erase');
   buttons.forEach(button => {
     button.classList.remove('selected');
   });
@@ -59,6 +60,20 @@ function handleOnClickUnique(clickedButton) {
   if ((isPen && isSelected) || (!isPen)) {
     deactivatePenTool();
   }
+  if (isErase && !isSelected) {
+    activateEraseTool();
+  }
+  if ((isErase && isSelected) || (!isErase)) {
+    deactivateEraseTool();
+  }
+}
+
+if (typeof mousedownHandler === 'undefined') {
+  var mousedownHandler;
+}
+
+if (typeof mouseupHandler === 'undefined') {
+  var mouseupHandler;
 }
 
 function activateLineTool() {
@@ -92,6 +107,15 @@ function deactivateLineTool() {
   canvas.removeEventListener('mouseup', mouseupHandler);
 }
 
+if (typeof PendownHandler === 'undefined') {
+  var PendownHandler;
+}
+if (typeof PenmoveHandler === 'undefined') {
+  var PenmoveHandler;
+}
+if (typeof PenupHandler === 'undefined') {
+  var PenupHandler;
+}
 function activatePenTool() {
   const canvas = document.getElementById('my-canvas');
   const ctx = canvas.getContext('2d');
@@ -135,4 +159,55 @@ function drawLine(ctx, x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
+}
+
+if (typeof eraseupHandler === 'undefined') {
+  var eraseupHandler;
+}
+if (typeof erasedownHandler === 'undefined') {
+  var erasedownHandler;
+}
+if (typeof erasemoveHandler === 'undefined') {
+  var erasemoveHandler;
+}
+function activateEraseTool() {
+  const canvas = document.getElementById('my-canvas');
+  const ctx = canvas.getContext('2d');
+  let startX;
+  let startY;
+  let isDrawing = false;
+  
+  erasedownHandler = (e) => {
+    isDrawing = true;
+    ctx.lineWidth = 20;
+    startX = e.clientX;
+    startY = e.clientY;
+    ctx.globalCompositeOperation = 'destination-out';
+  };
+
+  erasemoveHandler = (e) => {
+    if (isDrawing) {
+      drawLine(ctx, startX, startY, e.clientX, e.clientY);
+      startX = e.clientX;
+      startY = e.clientY;
+    }
+  };
+
+  eraseupHandler = (e) => {
+    if (isDrawing) {
+      isDrawing = false;
+      ctx.lineWidth = 1;
+      ctx.globalCompositeOperation = 'source-over';
+    }
+  };
+  canvas.addEventListener('mousedown', erasedownHandler);
+  canvas.addEventListener('mouseup', eraseupHandler);
+  canvas.addEventListener('mousemove', erasemoveHandler);
+}
+
+function deactivateEraseTool() {
+  const canvas = document.getElementById('my-canvas');
+  canvas.removeEventListener('mousedown', erasedownHandler);
+  canvas.removeEventListener('mouseup', eraseupHandler);
+  canvas.removeEventListener('mousemove', erasemoveHandler);
 }
